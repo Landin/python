374caedd6809e86d863c221bc7df72d23ae0962e
Landin <landinius84@gmail.com>
2014-08-15 18:02:31 +0200

    Serial work

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-import serial
+#!/usr/bin/env python
 
-ser = serial.Serial()
-ser.baudrate = 19200
-ser.port = 0
+import socket
 
-ser
 
-ser.open()
-ser.isopen()
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+MESSAGE = "Hello, World!"
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(MESSAGE)
+data = s.recv(BUFFER_SIZE)
+s.close()
+
+print "received data:", data
